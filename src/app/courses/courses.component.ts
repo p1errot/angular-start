6e27668af83d11428e29d9cc0e35d5aaed94d970
Samuel Rivera <samuelp1errot@gmail.com>
2018-02-27 10:30:33 +0800

    Basic - Pipes

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -10,6 +10,13 @@ import { CoursesService } from '../courses.service';
 export class CoursesComponent implements OnInit {
   title = "List of courses";
   courses;
+  course = {
+    title: "The test for Angular Course",
+    rating: 4.9745,
+    students: 30123,
+    price: 190.95,
+    releaseDate: new Date(2016, 3, 1)
+  };
 
   constructor(service: CoursesService) {
     this.courses = service.getCourses();
