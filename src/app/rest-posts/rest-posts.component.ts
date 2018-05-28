5b10eb81b981a4930126fc91fc3a7c3bb79e7e7b
Samuel Rivera <samuelp1errot@gmail.com>
2018-05-28 11:46:29 +0800

    Basic - REST create new item

--- src/app/rest-posts/rest-posts.component.ts
+++ src/app/rest-posts/rest-posts.component.ts
@@ -7,13 +7,26 @@ import { Http } from '@angular/http';
   styleUrls: ['./rest-posts.component.scss']
 })
 export class RestPostsComponent implements OnInit {
+  private url: string = 'http://jsonplaceholder.typicode.com/posts';
   posts: any[];
 
-  constructor(http: Http) {
-    http.get('http://jsonplaceholder.typicode.com/posts')
+  constructor(private http: Http) {
+    http.get(this.url)
       .subscribe(response => {
         this.posts = response.json();
       })
+  }
+
+  createPost(input: HTMLInputElement) {
+    let post = { title: input.value };
+
+    input.value = '';
+
+    this.http.post(this.url, JSON.stringify(post))
+      .subscribe(response => {
+        post['id'] = response.json().id;
+        this.posts.splice(0, 0, post);
+      });
   }
 
   ngOnInit() {
